8e6c446268a4e8804e0fd54570c83fdea4ed51b7
Joseph Bridger <josephbridger3@gmail.com>
2024-04-05 01:55:02 +0100

    code optimisations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
 
-    let p1 = Point {x: 10f32, y: 11f32, z: 12f32 };
-    let p2 = Point {x: 12f32, y: 24f32, z: 15f32 };
+    let p1 = DisplacementVector {x: 10f32, y: 11f32, z: 12f32 };
+    let p2 = DisplacementVector {x: 12f32, y: 24f32, z: 15f32 };
 
     let vector = Vector {origin: p1, head: p2};
 
@@ -21,14 +21,14 @@ fn main() {
 
 }
 
-struct Point { x: f32, y: f32, z: f32 }
+struct DisplacementVector { x: f32, y: f32, z: f32 }
 
-struct Vector { origin: Point, head: Point}
+struct Vector { origin: DisplacementVector, head: DisplacementVector }
 
 impl Vector {
     fn length (&self) -> f32{
-        let Point { x: x1, y: y1, z: z1} = self.origin;
-        let Point { x: x2, y: y2, z: z2} = self.head;
+        let DisplacementVector { x: x1, y: y1, z: z1} = self.origin;
+        let DisplacementVector { x: x2, y: y2, z: z2} = self.head;
 
         let x : f32 = x2 - x1;
         let y : f32 = y2 - y1;
@@ -38,8 +38,8 @@ impl Vector {
     }
 
     fn squared_length (&self) -> f32{
-        let Point { x: x1, y: y1, z: z1} = self.origin;
-        let Point { x: x2, y: y2, z: z2} = self.head;
+        let DisplacementVector { x: x1, y: y1, z: z1} = self.origin;
+        let DisplacementVector { x: x2, y: y2, z: z2} = self.head;
 
         let x : f32 = x2 - x1;
         let y : f32 = y2 - y1;
@@ -49,39 +49,36 @@ impl Vector {
     }
 
     fn normalise (&self) -> Vector{
-        let Point { x: x1, y: y1, z: z1} = self.origin;
-        let Point { x: x2, y: y2, z: z2} = self.head;
-
         let inverse_length : f32 = 1.0 / (&self).length();
 
-        Vector {
-            origin:
-            Point {x: x1, y: y1, z: z1},
-            head:
-            Point {
-                x: ((x2 - x1) * inverse_length) + x1,
-                y: ((y2 - y1) * inverse_length) + y1,
-                z: ((z2 - z1) * inverse_length) + z1,
-            }}
+        Self::normalised_vector(&self,inverse_length)
     }
 
     fn fast_normalise (&self) -> Vector{
-        let Point { x: x1, y: y1, z: z1} = self.origin;
-        let Point { x: x2, y: y2, z: z2} = self.head;
-
         let temp_length_squared:f32 = self.squared_length();
 
         let inverse_length : f32 = Vector::fast_inv_sqrt(temp_length_squared);
 
+        Self::normalised_vector(&self,inverse_length)
+    }
+
+    fn normalise_axis (axis1: f32, axis2: f32, inverse_length: f32) -> f32 {
+        ((axis2 - axis1) * inverse_length) + axis1
+    }
+
+    fn normalised_vector (&self, inverse_length: f32) -> Vector {
+        let DisplacementVector { x: x1, y: y1, z: z1} = self.origin;
+        let DisplacementVector { x: x2, y: y2, z: z2} = self.head;
+
         Vector {
             origin:
-                Point {x: x1, y: y1, z: z1},
+            DisplacementVector {x: x1, y: y1, z: z1},
             head:
-                Point {
-                x: ((x2 - x1) * inverse_length) + x1,
-                y: ((y2 - y1) * inverse_length) + y1,
-                z: ((z2 - z1) * inverse_length) + z1,
-        }}
+            DisplacementVector {
+                x: Self::normalise_axis(x1,x2,inverse_length),
+                y: Self::normalise_axis(y1,y2,inverse_length),
+                z: Self::normalise_axis(z1,z2,inverse_length)
+            }}
     }
 
     fn fast_inv_sqrt(x: f32) -> f32 {
