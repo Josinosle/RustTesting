119beda4861263cf40ece446419f3fee96cc9d6f
Josinosle <137291379+Josinosle@users.noreply.github.com>
2024-04-05 01:09:31 +0100

    Delete target directory

--- target/debug/deps/RustTesting-7f930fb8fc2da585.d
+++
@@ -1,5 +0,0 @@
-/var/home/josinosle/IdeaProjects/RustTesting/target/debug/deps/RustTesting-7f930fb8fc2da585: src/main.rs
-
-/var/home/josinosle/IdeaProjects/RustTesting/target/debug/deps/RustTesting-7f930fb8fc2da585.d: src/main.rs
-
-src/main.rs:
